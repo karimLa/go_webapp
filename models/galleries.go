ff8c2ee15ddb8ad4df323afe6e88ae93208b5aac
KarimLa <karimacc001@gmail.com>
2021-04-27 20:22:14 +0000

    Implemented the gallery validator struct

--- models/galleries.go
+++ models/galleries.go
@@ -8,14 +8,6 @@ type Gallery struct {
 	Title  string `gorm:"not null"`
 }
 
-func NewGalleryService(db *gorm.DB) GalleryService {
-	gg := &galleryGorm{db: db}
-
-	return galleryService{
-		GalleryDB: gg,
-	}
-}
-
 type GalleryDB interface {
 	Create(gallery *Gallery) error
 }
@@ -28,10 +20,35 @@ type galleryService struct {
 	GalleryDB
 }
 
+func NewGalleryService(db *gorm.DB) GalleryService {
+	gg := newGalleryGorm(db)
+	gv := newGalleryValidator(gg)
+
+	return &galleryService{
+		GalleryDB: gv,
+	}
+}
+
+type galleryValidator struct {
+	GalleryDB
+}
+
+func newGalleryValidator(gg *galleryGorm) *galleryValidator {
+	return &galleryValidator{
+		GalleryDB: gg,
+	}
+}
+
 type galleryGorm struct {
 	db *gorm.DB
 }
 
+func newGalleryGorm(db *gorm.DB) *galleryGorm {
+	return &galleryGorm{db: db}
+}
+
+// Create will create the provided gallery and backfill data
+// Like the ID, CreatedAt, and UpdatedAt fields.
 func (gg *galleryGorm) Create(g *Gallery) error {
-	return nil
+	return gg.db.Create(g).Error
 }
